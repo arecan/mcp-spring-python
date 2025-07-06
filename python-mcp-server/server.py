6a45df9bc2d1fcbd48c42c7b124e53341002eca8
arecan <nossanacera@gmail.com>
2025-07-06 08:01:50 +0100

    update user interface

--- python-mcp-server/server.py
+++ python-mcp-server/server.py
@@ -1,15 +1,19 @@
 from mcp.server.fastmcp import FastMCP
+import json
+
 mcp = FastMCP('Python-MCP-Server')
+
 @mcp.tool()
-def get_info_about(name : str) -> str:
+def get_info_about(name: str) -> str:
     """
-    Get Information about a given employee name:
+    Get information about a given employee name:
     - Name
     - Salary
     - Email
     """
-    return {
-        "name" :"Nacera",
-        "salary":50000,
-        "email":"nossanacera@gmail.com"
+    employee_info = {
+        "name": "Nacera",
+        "salary": 50000,
+        "email": "nossanacera@gmail.com"
     }
+    return json.dumps(employee_info)
